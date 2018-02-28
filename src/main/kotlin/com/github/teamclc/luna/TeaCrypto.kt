fd6ce2a3a2dc0d5f2b823bb9051cca4e6375eaed
Charlie Jiang <cqjjjzr@126.com>
2018-02-28 23:41:59 +0800

    修改某些变量名。
    Signed-off-by: Charlie Jiang <cqjjjzr@126.com>

--- src/main/kotlin/com/github/teamclc/luna/TeaCrypto.kt
+++ src/main/kotlin/com/github/teamclc/luna/TeaCrypto.kt
@@ -2,243 +2,243 @@ package com.github.teamclc.luna
 
 import javax.script.ScriptEngineManager
 
-private val jsEngine = ScriptEngineManager().getEngineByName("JS").also { it.eval(TEA_JS_CODE) }
+val jsEngine = ScriptEngineManager().getEngineByName("JS").also { it.eval(TEA_JS_CODE) }
 
 fun teaEncrypt(data: ByteArray, key: ByteArray) = jsEngine.eval("TEA.initkey('${bytesToHexString(key)}');TEA.encrypt('${bytesToHexString(data)}')") as String
 fun teaDecrypt(data: ByteArray, key: ByteArray) = jsEngine.eval("TEA.initkey('${bytesToHexString(key)}');TEA.decrypt('${bytesToHexString(data)}')") as String
 
 private const val TEA_JS_CODE = """
 var key = "",
-    a = 0,
-    h = [],
+    paddings = 0,
+    temp = [],
     z = [],
     A = 0,
     w = 0,
-    o = [],
+    result = [],
     v = [],
     p = true;
-  function getRandomNumber() {
-    return Math.round(Math.random() * 4294967295);
-  }
-  function k(E, F, B) {
+function getRandomNumber() {
+    return Math.round(Math.random() * 0xffffffff);
+}
+function k(E, F, B) {
     if (!B || B > 4) {
-      B = 4;
+        B = 4;
     }
     var C = 0;
     for (var D = F; D < F + B; D++) {
-      C <<= 8;
-      C |= E[D];
+        C <<= 8;
+        C |= E[D];
     }
-    return (C & 4294967295) >>> 0;
-  }
-  function b(C, D, B) {
-    C[D + 3] = (B >> 0) & 255;
-    C[D + 2] = (B >> 8) & 255;
-    C[D + 1] = (B >> 16) & 255;
-    C[D + 0] = (B >> 24) & 255;
-  }
-  function bytesToHexString(bytes) {
+    return (C & 0xffffffff) >>> 0;
+}
+function b(C, D, B) {
+    C[D + 3] = (B >> 0) & 0xFF;
+    C[D + 2] = (B >> 8) & 0xFF;
+    C[D + 1] = (B >> 16) & 0xFF;
+    C[D + 0] = (B >> 24) & 0xFF;
+}
+function bytesToHexString(bytes) {
     if (!bytes) {
-      return "";
+        return "";
     }
     var result = "";
     for (var i = 0; i < bytes.length; i++) {
-      var curByte = Number(bytes[i]).toString(16);
-      if (curByte.length == 1) {
-        curByte = "0" + curByte;
-      }
-      result += curByte;
+        var curByte = Number(bytes[i]).toString(16);
+        if (curByte.length == 1) {
+            curByte = "0" + curByte;
+        }
+        result += curByte;
     }
     return result;
-  }
-  function x(C) {
+}
+function x(C) {
     var D = "";
     for (var B = 0; B < C.length; B += 2) {
-      D += String.fromCharCode(parseInt(C.substr(B, 2), 16));
+        D += String.fromCharCode(parseInt(C.substr(B, 2), 16));
     }
     return D;
-  }
-  function c(E, B) {
+}
+function c(E, B) {
     if (!E) {
-      return "";
+        return "";
     }
     if (B) {
-      E = m(E);
+        E = m(E);
     }
     var D = [];
     for (var C = 0; C < E.length; C++) {
-      D[C] = E.charCodeAt(C);
+        D[C] = E.charCodeAt(C);
     }
     return bytesToHexString(D);
-  }
-  function m(E) {
+}
+function m(E) {
     var D,
-      F,
-      C = [],
-      B = E.length;
+        F,
+        C = [],
+        B = E.length;
     for (D = 0; D < B; D++) {
-      F = E.charCodeAt(D);
-      if (F > 0 && F <= 127) {
-        C.push(E.charAt(D));
-      } else {
-        if (F >= 128 && F <= 2047) {
-          C.push(
-            String.fromCharCode(192 | ((F >> 6) & 31)),
-            String.fromCharCode(128 | (F & 63))
-          );
+        F = E.charCodeAt(D);
+        if (F > 0 && F <= 127) {
+            C.push(E.charAt(D));
         } else {
-          if (F >= 2048 && F <= 65535) {
-            C.push(
-              String.fromCharCode(224 | ((F >> 12) & 15)),
-              String.fromCharCode(128 | ((F >> 6) & 63)),
-              String.fromCharCode(128 | (F & 63))
-            );
-          }
+            if (F >= 128 && F <= 2047) {
+                C.push(
+                    String.fromCharCode(192 | ((F >> 6) & 31)),
+                    String.fromCharCode(128 | (F & 63))
+                );
+            } else {
+                if (F >= 2048 && F <= 65535) {
+                    C.push(
+                        String.fromCharCode(224 | ((F >> 12) & 15)),
+                        String.fromCharCode(128 | ((F >> 6) & 63)),
+                        String.fromCharCode(128 | (F & 63))
+                    );
+                }
+            }
         }
-      }
     }
     return C.join("");
-  }
-  function encryptBytes(D) {
-    h = new Array(8);
+}
+function encryptBytes(input) {
+    temp = new Array(8);
     z = new Array(8);
     A = w = 0;
     p = true;
-    a = 0;
-    var B = D.length;
+    paddings = 0;
+    var inputLen = input.length;
     var E = 0;
-    a = (B + 10) % 8;
-    if (a != 0) {
-      a = 8 - a;
+    paddings = (inputLen + 10) % 8;
+    if (paddings != 0) {
+        paddings = 8 - paddings;
     }
-    o = new Array(B + a + 10);
-    h[0] = ((getRandomNumber() & 248) | a) & 255;
-    for (var C = 1; C <= a; C++) {
-      h[C] = getRandomNumber() & 255;
+    result = new Array(inputLen + paddings + 10);
+    temp[0] = ((getRandomNumber() & 0xF8) | paddings) & 0xFF;
+    for (var i = 1; i <= paddings; i++) {
+        temp[i] = getRandomNumber() & 0xFF;
     }
-    a++;
-    for (var C = 0; C < 8; C++) {
-      z[C] = 0;
+    paddings++;
+    for (var i = 0; i < 8; i++) {
+        z[i] = 0;
     }
     E = 1;
     while (E <= 2) {
-      if (a < 8) {
-        h[a++] = getRandomNumber() & 255;
-        E++;
-      }
-      if (a == 8) {
-        QQCBC();
-      }
+        if (paddings < 8) {
+            temp[paddings++] = getRandomNumber() & 0xFF;
+            E++;
+        }
+        if (paddings == 8) {
+            QQCBC();
+        }
     }
-    var C = 0;
-    while (B > 0) {
-      if (a < 8) {
-        h[a++] = D[C++];
-        B--;
-      }
-      if (a == 8) {
-        QQCBC();
-      }
+    var i = 0;
+    while (inputLen > 0) {
+        if (paddings < 8) {
+            temp[paddings++] = input[i++];
+            inputLen--;
+        }
+        if (paddings == 8) {
+            QQCBC();
+        }
     }
     E = 1;
     while (E <= 7) {
-      if (a < 8) {
-        h[a++] = 0;
-        E++;
-      }
-      if (a == 8) {
-        QQCBC();
-      }
+        if (paddings < 8) {
+            temp[paddings++] = 0;
+            E++;
+        }
+        if (paddings == 8) {
+            QQCBC();
+        }
     }
-    return o;
-  }
-  function decryptBytes(F) {
+    return result;
+}
+function decryptBytes(F) {
     var E = 0;
     var C = new Array(8);
     var B = F.length;
     v = F;
     if (B % 8 != 0 || B < 16) {
-      return null;
+        return null;
     }
     z = teaDecrypt(F);
-    a = z[0] & 7;
-    E = B - a - 10;
+    paddings = z[0] & 7;
+    E = B - paddings - 10;
     if (E < 0) {
-      return null;
+        return null;
     }
     for (var D = 0; D < C.length; D++) {
-      C[D] = 0;
+        C[D] = 0;
     }
-    o = new Array(E);
+    result = new Array(E);
     w = 0;
     A = 8;
-    a++;
+    paddings++;
     var G = 1;
     while (G <= 2) {
-      if (a < 8) {
-        a++;
-        G++;
-      }
-      if (a == 8) {
-        C = F;
-        if (!g()) {
-          return null;
+        if (paddings < 8) {
+            paddings++;
+            G++;
         }
-      }
+        if (paddings == 8) {
+            C = F;
+            if (!g()) {
+                return null;
+            }
+        }
     }
     var D = 0;
     while (E != 0) {
-      if (a < 8) {
-        o[D] = (C[w + a] ^ z[a]) & 255;
-        D++;
-        E--;
-        a++;
-      }
-      if (a == 8) {
-        C = F;
-        w = A - 8;
-        if (!g()) {
-          return null;
+        if (paddings < 8) {
+            result[D] = (C[w + paddings] ^ z[paddings]) & 0xFF;
+            D++;
+            E--;
+            paddings++;
         }
-      }
+        if (paddings == 8) {
+            C = F;
+            w = A - 8;
+            if (!g()) {
+                return null;
+            }
+        }
     }
     for (G = 1; G < 8; G++) {
-      if (a < 8) {
-        if ((C[w + a] ^ z[a]) != 0) {
-          return null;
+        if (paddings < 8) {
+            if ((C[w + paddings] ^ z[paddings]) != 0) {
+                return null;
+            }
+            paddings++;
         }
-        a++;
-      }
-      if (a == 8) {
-        C = F;
-        w = A;
-        if (!g()) {
-          return null;
+        if (paddings == 8) {
+            C = F;
+            w = A;
+            if (!g()) {
+                return null;
+            }
         }
-      }
     }
-    return o;
-  }
-  //交织算法CBC加密
-  function QQCBC() {
+    return result;
+}
+//交织算法CBC加密
+function QQCBC() {
     for (var i = 0; i < 8; i++) {
-      if (p) {
-        h[i] ^= z[i];
-      } else {
-        h[i] ^= o[w + i];
-      }
+        if (p) {
+            temp[i] ^= z[i];
+        } else {
+            temp[i] ^= result[w + i];
+        }
     }
-    var C = teaEncrypt(h);
+    var C = teaEncrypt(temp);
     for (var i = 0; i < 8; i++) {
-      o[A + i] = C[i] ^ z[i];
-      z[i] = h[i];
+        result[A + i] = C[i] ^ z[i];
+        z[i] = temp[i];
     }
     w = A;
     A += 8;
-    a = 0;
+    paddings = 0;
     p = false;
-  }
-  function teaEncrypt(B) {
+}
+function teaEncrypt(B) {
     var C = 16;
     var H = k(B, 0, 4);
     var G = k(B, 4, 4);
@@ -247,93 +247,94 @@ var key = "",
     var F = k(key, 8, 4);
     var E = k(key, 12, 4);
     var D = 0;
-    var K = 2654435769 >>> 0;
+    var K = 0x9e3779b9 >>> 0;
     while (C-- > 0) {
-      D += K;
-      D = (D & 4294967295) >>> 0;
-      H += ((G << 4) + J) ^ (G + D) ^ ((G >>> 5) + I);
-      H = (H & 4294967295) >>> 0;
-      G += ((H << 4) + F) ^ (H + D) ^ ((H >>> 5) + E);
-      G = (G & 4294967295) >>> 0;
+        D += K;
+        D = (D & 0xffffffff) >>> 0;
+        H += ((G << 4) + J) ^ (G + D) ^ ((G >>> 5) + I);
+        H = (H & 0xffffffff) >>> 0;
+        G += ((H << 4) + F) ^ (H + D) ^ ((H >>> 5) + E);
+        G = (G & 0xffffffff) >>> 0;
     }
     var L = new Array(8);
     b(L, 0, H);
     b(L, 4, G);
     return L;
-  }
-  function teaDecrypt(B) {
-    var C = 16;
-    var H = k(B, 0, 4);
-    var G = k(B, 4, 4);
+}
+function teaDecrypt(bytes) {
+    var remainingRounds = 16;
+    var H = k(bytes, 0, 4);
+    var G = k(bytes, 4, 4);
     var J = k(key, 0, 4);
     var I = k(key, 4, 4);
     var F = k(key, 8, 4);
     var E = k(key, 12, 4);
-    var D = 3816266640 >>> 0;
-    var K = 2654435769 >>> 0;
-    while (C-- > 0) {
-      G -= ((H << 4) + F) ^ (H + D) ^ ((H >>> 5) + E);
-      G = (G & 4294967295) >>> 0;
-      H -= ((G << 4) + J) ^ (G + D) ^ ((G >>> 5) + I);
-      H = (H & 4294967295) >>> 0;
-      D -= K;
-      D = (D & 4294967295) >>> 0;
+    var D = 0xe3779b90 >>> 0;
+    var K = 0x9e3779b9 >>> 0;
+    while (remainingRounds-- > 0) {
+        G -= ((H << 4) + F) ^ (H + D) ^ ((H >>> 5) + E);
+        G = (G & 0xffffffff) >>> 0;
+        H -= ((G << 4) + J) ^ (G + D) ^ ((G >>> 5) + I);
+        H = (H & 0xffffffff) >>> 0;
+        D -= K;
+        D = (D & 0xffffffff) >>> 0;
     }
     var L = new Array(8);
     b(L, 0, H);
     b(L, 4, G);
     return L;
-  }
-  function g() {
+}
+function g() {
     var B = v.length;
-    for (var C = 0; C < 8; C++) {
-      z[C] ^= v[A + C];
+    for (var i = 0; i < 8; i++) {
+        z[i] ^= v[A + i];
     }
     z = teaDecrypt(z);
     A += 8;
-    a = 0;
+    paddings = 0;
     return true;
-  }
-  function stringToBytes(input, isCharString) {
+}
+function stringToBytes(input, isCharString) {
     var result = [];
     if (isCharString) {
-      for (var i = 0; i < input.length; i++) {
-        result[i] = input.charCodeAt(i) & 255; // 取ASCII转换
-      }
+        for (var i = 0; i < input.length; i++) {
+            result[i] = input.charCodeAt(i) & 0xFF; // 取ASCII转换
+        }
     } else {
-      var bytePos = 0;
-      for (var i = 0; i < input.length; i += 2) {
-        result[bytePos++] = parseInt(input.substr(i, 2), 16); // 视为16进制字符串转换
-      }
+        var bytePos = 0;
+        for (var i = 0; i < input.length; i += 2) {
+            result[bytePos++] = parseInt(input.substr(i, 2), 16); // 视为16进制字符串转换
+        }
     }
     return result;
-  }
-  TEA = {
+}
+TEA = {
     encrypt: function(input, isCharString) {
-      var stringBytes = stringToBytes(input, isCharString);
-      var resultBytes = encryptBytes(stringBytes);
-      return bytesToHexString(resultBytes);
+        var stringBytes = stringToBytes(input, isCharString);
+        var resultBytes = encryptBytes(stringBytes);
+        return bytesToHexString(resultBytes);
     },
     encryptAsBase64: function(input, isCharString) {
-      var stringBytes = stringToBytes(input, isCharString);
-      var resultBytes = encryptBytes(stringBytes);
-      var rawString = "";
-      for (var i = 0; i < resultBytes.length; i++) {
-        rawString += String.fromCharCode(resultBytes[i]);
-      }
-      return btoa(rawString);
+        var stringBytes = stringToBytes(input, isCharString);
+        var resultBytes = encryptBytes(stringBytes);
+        var rawString = "";
+        for (var i = 0; i < resultBytes.length; i++) {
+            rawString += String.fromCharCode(resultBytes[i]);
+        }
+        return btoa(rawString);
     },
     decrypt: function(input) {
-      var stringBytes = stringToBytes(input, false);
-      var resultBytes = decryptBytes(stringBytes);
-      return bytesToHexString(resultBytes);
+        var stringBytes = stringToBytes(input, false);
+        var resultBytes = decryptBytes(stringBytes);
+        return bytesToHexString(resultBytes);
     },
     initkey: function(input, isCharString) {
-      key = stringToBytes(input, isCharString);
+        key = stringToBytes(input, isCharString);
     },
     bytesToStr: x,
     strToBytes: c,
     bytesInStr: bytesToHexString,
     dataFromStr: stringToBytes
-  };
+};
+
         """
